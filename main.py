85ef4fce563791dac4de841524d1103f1b4d6ae2
jeetdave2000 <87813042+jeetdave2000@users.noreply.github.com>
2025-04-19 19:32:32 +0530

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import numpy as np
 
 #Tensorflow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_model.h5")
-    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(64,64))
+    model = tf.keras.models.load_model("mobilenet.h5")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
     predictions = model.predict(input_arr)
