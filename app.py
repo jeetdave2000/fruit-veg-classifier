b9f4721410a47b9d7afdc9d135e285ec72ae34d4
jeetdave2000 <87813042+jeetdave2000@users.noreply.github.com>
2025-04-19 20:49:25 +0530

    app.py

--- app.py
+++ app.py
@@ -1,76 +1,75 @@
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.utils import custom_object_scope
-from tensorflow.keras.layers import Layer
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from PIL import Image
+import requests
+from bs4 import BeautifulSoup
 import numpy as np
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
 
-# Print TensorFlow version for debugging
-st.write(f"TensorFlow Version: {tf.__version__}")
+model = load_model('FV.h5')
 
-# Define a custom TrueDivide layer
-class TrueDivideLayer(Layer):
-    def __init__(self, **kwargs):
-        super(TrueDivideLayer, self).__init__(**kwargs)
+labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
+          7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
+          14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
+          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',
+          26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
+          32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
 
-    def call(self, inputs):
-        # Handle single input (e.g., input / constant) or two inputs (x / y)
-        if isinstance(inputs, (list, tuple)):
-            x, y = inputs
-            return tf.math.truediv(x, y)
-        else:
-            # Assume division by a constant (e.g., 127.5 for MobileNetV2 preprocessing)
-            return tf.math.truediv(inputs, tf.constant(127.5, dtype=inputs.dtype))
+fruits = ['Apple', 'Banana', 'Bello Pepper', 'Chilli Pepper', 'Grapes', 'Jalepeno', 'Kiwi', 'Lemon', 'Mango', 'Orange',
+          'Paprika', 'Pear', 'Pineapple', 'Pomegranate', 'Watermelon']
+vegetables = ['Beetroot', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Corn', 'Cucumber', 'Eggplant', 'Ginger',
+              'Lettuce', 'Onion', 'Peas', 'Potato', 'Raddish', 'Soy Beans', 'Spinach', 'Sweetcorn', 'Sweetpotato',
+              'Tomato', 'Turnip']
 
-    def get_config(self):
-        config = super(TrueDivideLayer, self).get_config()
-        return config
 
-# Function to load and predict
-@st.cache_resource
-def load_model():
+def fetch_calories(prediction):
     try:
-        with custom_object_scope({'TrueDivide': TrueDivideLayer}):
-            model = tf.keras.models.load_model("FV.h5", compile=False)
-        return model
+        url = 'https://www.google.com/search?&q=calories in ' + prediction
+        req = requests.get(url).text
+        scrap = BeautifulSoup(req, 'html.parser')
+        calories = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
+        return calories
     except Exception as e:
-        st.error(f"Error loading model: {str(e)}")
-        return None
+        st.error("Can't able to fetch the Calories")
+        print(e)
 
-def model_prediction(image):
-    model = load_model()
-    if model is None:
-        return None
-    img = load_img(image, target_size=(128, 128))
-    img_array = img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
-    predictions = model.predict(img_array)
-    return np.argmax(predictions[0])
 
-# Class names (based on your dataset with 36 classes)
-class_names = [
-    'apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot',
-    'cauliflower', 'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger',
-    'grapes', 'jalapeno', 'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 'orange',
-    'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'raddish',
-    'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon'
-]
+def prepare_image(img_path):
+    img = load_img(img_path, target_size=(224, 224, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res.capitalize()
 
-# Streamlit app interface
-st.title("Fruit and Vegetable Classifier")
-st.write("Upload an image of a fruit or vegetable to classify it.")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    # Display the uploaded image
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-    
-    # Predict
-    result_index = model_prediction(uploaded_file)
-    if result_index is not None:
-        result = class_names[result_index]
-        st.write(f"Prediction: **{result}**")
-    else:
-        st.error("Failed to make a prediction due to model loading error.")
+def run():
+    st.title("Fruits🍍-Vegetable🍅 Classification")
+    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    if img_file is not None:
+        img = Image.open(img_file).resize((250, 250))
+        st.image(img, use_column_width=False)
+        save_image_path = './upload_images/' + img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
+
+        # if st.button("Predict"):
+        if img_file is not None:
+            result = prepare_image(save_image_path)
+            if result in vegetables:
+                st.info('**Category : Vegetables**')
+            else:
+                st.info('**Category : Fruit**')
+            st.success("**Predicted : " + result + '**')
+            cal = fetch_calories(result)
+            if cal:
+                st.warning('**' + cal + '(100 grams)**')
+
+
+run()
